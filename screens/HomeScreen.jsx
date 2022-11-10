d7cceb401190e0bb37159c74610e720dfb761017
Paul Wang <paul@brivity.com>
2022-11-09 21:17:56 -0800

    add header to homescreen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,6 +1,12 @@
-import { SafeAreaView, Text, View, Image } from "react-native";
+import { SafeAreaView, Text, View, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
 
 export const HomeScreen = () => {
   const navigation = useNavigation();
@@ -12,16 +18,32 @@ export const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView className="bg-white pt-5">
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image
           source={{ uri: "https://links.papareact.com/wru" }}
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
-        <View>
-          <Text>Deliver Now</Text>
-          <Text>Current Location</Text>
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size="20" color="#00CCBB" />
+          </Text>
         </View>
+        <UserIcon size="35" color="#00CCBB" />
+      </View>
+      {/* Search */}
+      <View className="flex-row px-4 space-x-2 pb-2 items-center">
+        <View className="flex-row flex-1 space-x-2 p-3 first-line:bg-gray-200 ">
+          <MagnifyingGlassIcon size="20" color="gray" />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            className="flex-1"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
       </View>
     </SafeAreaView>
   );
